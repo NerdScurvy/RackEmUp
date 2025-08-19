456789517126bd85b1da86113f12ee6ccfa92c69
NerdScurvy <NerdScurvy@gmail.com>
2025-08-19 15:43:57 +0800

    Fix Core rename

--- src/Core.cs
+++ src/Core.cs
@@ -1,21 +1,28 @@
+using CarryOn.API.Common;
 using Vintagestory.API.Common;
 
-[assembly: ModInfo("Carry On Mold Rack Transfer",
-    modID: "carryonmoldracktransfer",
+[assembly: ModInfo("CarryOn RackEmUp",
+    modID: "rackemup",
     Version = "1.0.0",
-    Description = "Adds the capability to transfer molds to and from the mold rack",
-    Website = "https://github.com/NerdScurvy/CarryOnMoldRackTransfer",
+    Description = "Adds the capability to transfer carryable molds to and from the mold rack",
+    Website = "https://github.com/NerdScurvy/RackEmUp",
     Authors = new[] { "NerdScurvy" })]
-[assembly: ModDependency("game", "1.21.0-rc.4")]
+[assembly: ModDependency("game", "1.21.0-rc.6")]
 
-namespace CarryOn.MoldRackTransfer
+namespace CarryOn.RackEmUp
 {
     /// <summary> Main system for the "Carry On" mod, which allows certain
     ///           blocks such as chests to be picked up and carried around. </summary>
     public class Core : ModSystem
     { 
+
+        public ICarryManager CarryManager { get; set; }
+        
         public override void Start(ICoreAPI api)
         {
+            var carryOnLib = api.ModLoader.GetModSystem<CarryOnLib.Core>();
+            CarryManager = carryOnLib.CarryManager;
+
             api.RegisterBlockBehaviorClass(BlockBehaviorMoldRackTransfer.Name, typeof(BlockBehaviorMoldRackTransfer));
 
         }
